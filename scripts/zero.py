8d099b67d67e90e1ca768a3879fcc7f6ce6213a7
AntonioDRSousa <53109191+AntonioDRSousa@users.noreply.github.com>
2024-04-27 00:55:51 -0300

    Add files via upload

--- scripts/zero.py
+++ scripts/zero.py
@@ -1,9 +1,12 @@
+nhits = [0]*252
+nnum = [0]*252
+
 fp1=open("nhits.txt","w")
 fp2=open("nnum.txt","w")
 
 for i in range(0,252):
-    fp1.write("0\n")
-    fp2.write("0\n")
+    fp1.write(str(nhits[i])+"\n")
+    fp2.write(str(nnum[i])+"\n")
 
 fp1.close()
 fp2.close()
